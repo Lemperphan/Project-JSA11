7568d71b05966389aa25d517ccbde31c0807b78a
unknown <falkunboy15@gmail.com>
2022-10-09 09:06:28 +0700

    update

--- Signup_page/Signup.js
+++ Signup_page/Signup.js
@@ -7,27 +7,26 @@ function submit() {
     var Mail = document.getElementById('mail')
     var Pw = document.getElementById('Pw')
 
-    if(Aname.value.length == 0){
+    if (Aname.value.length == 0) {
         alert('Please enter your account name');
 
-    }else if(Fname.value.length == 0){
+    } else if (Fname.value.length == 0) {
         alert('Please enter your first name');
-    }
-    else if(Lname.value.length == 0){
+    } else if (Lname.value.length == 0) {
         alert('Please enter your last name');
     }
-    else if(Mail.value.length == 0){
+    else if (Mail.value.length == 0) {
         alert('Please enter your email');
     }
-    else if(Pw.value.length == 0){
+    else if (Pw.value.length == 0) {
         alert('Please enter password');
-    }else{
+    } else {
         const user_acc = {
-            acc_name : Aname.value,
-            first_name : Fname.value,
-            last_name : Lname.value,
-            mail : Mail.value,
-            password : Pw.value,
+            acc_name: Aname.value,
+            first_name: Fname.value,
+            last_name: Lname.value,
+            mail: Mail.value,
+            password: Pw.value,
         }
         window.localStorage.setItem("user_acc", JSON.stringify(user_acc));
         window.location.href = "../Login_page/login.html"
